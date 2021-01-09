ac75e2afb9389a7c9016434158390f426dfcda9a
Alex Chung <alex.chung92@gmail.com>
2021-01-09 15:45:12 -0500

    feature/drag drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,6 +175,66 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// draggables
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("over", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){    
+    // add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    });
+    });
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+    
+    var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+    
+    var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+  console.log(tempArr);
+  }
+});
+
+$("#trash").droppable({
+
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+
+});
 // load tasks for the first time
 loadTasks();
 
